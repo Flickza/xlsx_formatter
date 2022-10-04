bf2f1d67814cf000cd2c1c0c51d77c360d0f6a63
Adel Johan Holm Gundersen <58791043+Flickza@users.noreply.github.com>
2022-10-04 13:41:14 +0200

    ...

--- src/renderer/Functions/GetYear.ts
+++ src/renderer/Functions/GetYear.ts
@@ -2,6 +2,10 @@ const GetYear = (fdato?: string, pnr?: string, def?: string) => {
   if (pnr === undefined || fdato === undefined || fdato === '' || pnr === '') {
     return def;
   }
+  // get 2 last characters of fdato
+  const lastTwo = fdato.slice(-2);
+  const currentYear: string = new Date().getFullYear().toString();
+
   let year;
   // 1900 - 1999
   if (/^([0-4]\d\d)/g.test(pnr.toString().substring(0, 3))) {
@@ -9,7 +13,12 @@ const GetYear = (fdato?: string, pnr?: string, def?: string) => {
   }
   // 2000 - 2099
   if (/^([5-9]\d\d)/g.test(pnr.toString().substring(0, 3))) {
-    year = 20;
+    // check if age would be greater than current year
+    if (`20${lastTwo}` > currentYear) {
+      year = 18;
+    } else {
+      year = 20;
+    }
   }
   return year;
 };
